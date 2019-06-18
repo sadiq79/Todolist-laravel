1534dc2e60fb9313366376b4e155514a7f876baa
sadiq79 <sadiq.md.ali@gmail.com>
2019-06-18 18:00:56 +0530

    2todo

---
+++ resources/views/components/taskForm.blade.php
@@ -0,0 +1,19 @@
+{!! Form::open(['route' => 'task.store','method' => 'POST']) !!}
+
+	{{ Form::label('name', 'Task Name',['class' => 'control-label']) }}
+	{{ Form::text('name', null, ['class' => 'form-control form-control-lg','placeholder' => 'Task Name'])  }}
+
+
+	{{ Form::label('description', 'Task Description',['class' => 'control-label mt-3']) }}
+	{{ Form::textarea('description', null,['class' => 'form-control','placeholder' => 'Task description'])  }}
+
+	{{ Form::label('date', 'Due Date',['class' => 'control-label mt-3']) }}
+	{{ Form::date('date', \Carbon\Carbon::now(),['class' => 'form-control']) }}
+
+	<div class="row justify-content-center ">
+		<div class="col-sm-6">
+			<button class="btn-btn-block btn-success" type="submit">Create Task</button>
+		</div>
+	</div>
+
+{!! Form::close()  !!}
